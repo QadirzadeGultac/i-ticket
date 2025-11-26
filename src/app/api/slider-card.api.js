93bf2691762af4827ac15c0909b8cd398588e971
QadirzadeGultac <gultac.qadirzade@mail.ru>
2025-11-26 14:49:27 +0400

    deploy test

--- src/app/api/slider-card.api.js
+++
@@ -1,4 +0,0 @@
-import { getPromotion } from "@/app/lib/http";
-export const fetchPromotionById = async (index, itemIndex) => {
-    return await getPromotion(index, itemIndex);
-};
--- src/ui/modules/home/Home.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import SliderCard from './cards/SliderCard';
-// import Header from '../header/Header'
-const Home = () => {
-    return (_jsx("div", { children: _jsx(SliderCard, {}) }));
-};
-export default Home;
